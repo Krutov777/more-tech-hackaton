e17ca5eea43f141a9546cec062dc578822eb5a72
Vladislav-ux <vladik201518@gmail.com>
2023-10-13 19:07:31 +0300

    init commit

---
+++ src/main/kotlin/com/moretech/MoretechApplication.kt
@@ -0,0 +1,11 @@
+package com.moretech
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoretechApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoretechApplication>(*args)
+}
---
+++ src/test/kotlin/com/moretech/MoretechApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moretech
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoretechApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
